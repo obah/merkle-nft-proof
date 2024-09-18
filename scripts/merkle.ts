55ff8a983e43f8b92e051bb1bd42212d6ab9ad23
obah <olusojiobah@gmail.com>
2024-09-18 17:45:16 +0100

    feat: implement merkle.ts

---
+++ scripts/merkle.ts
@@ -0,0 +1,36 @@
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import fs from "fs";
+import csv from "csv-parser";
+
+const values: any = [];
+fs.createReadStream("./lib/eligibleAccounts.csv")
+  .pipe(csv())
+  .on("data", (row: { address: string; amount: string }) => {
+    values.push([row.address, row.amount]);
+  })
+  .on("end", () => {
+    const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
+    console.log("Merkle Root:", tree.root);
+
+    fs.writeFileSync("tree.json", JSON.stringify(tree.dump()));
+
+    const proofs = {};
+
+    try {
+      const loadedTree = StandardMerkleTree.load(
+        JSON.parse(fs.readFileSync("tree.json", "utf8"))
+      );
+      for (const [i, v] of loadedTree.entries()) {
+        const proof: any = loadedTree.getProof(i);
+        proofs[v[0]] = proof;
+      }
+
+      fs.writeFileSync("proofs.json", JSON.stringify(proofs, null, 2));
+      console.log("All proofs have been saved to 'proofs.json'.");
+    } catch (err) {
+      console.error("Error reading or processing 'tree.json':", err);
+    }
+  })
+  .on("error", (err) => {
+    console.error("Error reading 'airdrop.csv':", err);
+  });
